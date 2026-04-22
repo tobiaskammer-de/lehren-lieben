9d7e99619256e200c8c4798adabe8ed97bb947b8
Tobias Kammer <smartwaerts@gmail.com>
2026-04-22 19:42:07 +0200

    Wire Newsletter buttons to Brevo signup form

---
+++ src/lib/links.ts
@@ -0,0 +1,8 @@
+/**
+ * External links used in multiple places.
+ * Centralized so they can be updated in one spot.
+ */
+
+/** Brevo/Sendinblue hosted newsletter signup form */
+export const NEWSLETTER_URL =
+  'https://a11acbc0.sibforms.com/serve/MUIFAJDlDVQE_kchmoLxexQuctqqpYtx71UV0LbgdBKmTLu-v2gGL9tHWxiQPjKJVnJ1Txy-0zqBA7Lb4VGpbC-kGXRwkvELUODuZy_rcxfPCeDOmWCYn_vc1kFsgIfRsS7QwJ5dJ4FFCF_RBmc_-xnD2UaNTAauhz1EsvLJVkUwla5-uECm5AjaQZLEgPpOsaZvNhWFVcXqgChZzA==';
